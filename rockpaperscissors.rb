5c58e5bbd9af219ca28047fd8c21f49c9b3f5d90
Anesha S Jones <anesha.s.jones@gmail.com>
2016-09-27 15:22:23 -0700

    Rock Paper Scissors Excercise/TTS Fall 2016

---
+++ rockpaperscissors.rb
@@ -0,0 +1,29 @@
+rps_array = ["Rock", "Paper", "Scissors"]
+
+
+user_choice = ["rock", "paper", "scissors"]
+computer_weapons= ["rock", "paper", "scissors"].sample
+puts "Please select rock, paper or scissors"
+user_choice= gets.chomp.downcase
+if user_choice != "rock", "paper", "scissors"
+  Puts "try again."
+if user_choice= "rock","paper", "scissors"
+  Puts "Its the computer's turn"
+Puts "computer_weapons is  " + computer_weapons
+end
+
+while (user_choice=="rock"||"paper"||"scissors")
+elsif (user_choice== "paper" && computer_weapons= rps_array [2])
+    puts "computer selected scissors! computer won!"
+  elsif (user_choice== "rock" && computer_weapons= rps_array [1])
+    puts "computer selected paper! computer won!"
+  elsif (user_choice== "scissors") && computer_weapons= rps_array [0])
+    puts "computer selected rock! computer won!"
+  elsif (user_choice== "scissors" && computer_weapons= rps_array [2])
+    puts "you won!"
+  elsif (user_choice== "rock" && computer_weapons= rps_array [1])
+    puts "you won!"
+  elsif (user_choice== "paper") && computer_weapons= rps_array [0])
+    puts "you won!"
+    if (user_choice==computer_weapons)
+      puts "we are tied!"
